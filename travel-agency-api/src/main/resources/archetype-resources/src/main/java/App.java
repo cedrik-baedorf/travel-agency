863d13b58dcf5eee5b0ad32d3118e957fb47de33
I551381 <cedrik.baedorf@sap.com>
2023-05-03 19:58:15 +0200

    Initial commit

---
+++ travel-agency-api/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $travel.agency;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
