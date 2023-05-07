dc605bbc27b8468a4d9afc5bca5667bb5a9cbd41
I551381 <cedrik.baedorf@sap.com>
2023-05-07 23:51:45 +0200

    change

--- travel-agency-service/src/test/java/travelagency/service/database/TravelAgencyServiceFactoryImplementationTest.java
+++ travel-agency-service/src/test/java/travelagency/service/database/TravelAgencyServiceFactoryImplementationTest.java
@@ -9,22 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TravelAgencyServiceFactoryImplementationTest {
 
-  @Test
-  public void testConstructor() {
-    Map<String, String> loginCredentials = new HashMap<>();
-    loginCredentials.put("javax.persistence.jdbc.user", "DEMO_USER");
-    loginCredentials.put("javax.persistence.jdbc.password", "PASSWORD");
-    assertDoesNotThrow(() -> new TravelAgencyServiceFactoryImplementation(loginCredentials));
-  }
-
-  @Test
-  public void testCreateEntityManager() {
-    Map<String, String> loginCredentials = new HashMap<>();
-    loginCredentials.put("javax.persistence.jdbc.user", "DEMO_USER");
-    loginCredentials.put("javax.persistence.jdbc.password", "PASSWORD");
-    assertDoesNotThrow(() -> new TravelAgencyServiceFactoryImplementation(loginCredentials));
-    assertDoesNotThrow(() -> new TravelAgencyServiceFactoryImplementation(loginCredentials).createEntityManager());
-    assertNotEquals(null, new TravelAgencyServiceFactoryImplementation(loginCredentials).createEntityManager());
-  }
+//diese klasse enthält keine Tests, da sie die Schnittstelle zur Datenbank ist und somit keine junit tests benötigt
 
 }
